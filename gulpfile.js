ee64f296e05d6951b0fce5c9e596e320251092ef
gilren <gillet.ren@gmail.com>
2022-02-18 15:46:44 +0100

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var min,
+var sass,
+  min,
   bases,
   path,
   opts,
@@ -10,198 +11,195 @@ var min,
   del,
   attrsSorter,
   posthtmlConfig,
-  reload
+  reload;
 
 /* ========================================================================
  *
  * Configuration
  * ======================================================================== */
 
-gulp = require('gulp')
-$ = require('gulp-load-plugins')()
-browserSync = require('browser-sync')
-postcss = require('gulp-postcss')
-autoprefixer = require('autoprefixer')
-attrsSorter = require('posthtml-attrs-sorter')
-del = require('del')
-reload = browserSync.reload
+sass = require("gulp-sass")(require("sass"));
+gulp = require("gulp");
+$ = require("gulp-load-plugins")();
+browserSync = require("browser-sync");
+postcss = require("gulp-postcss");
+autoprefixer = require("autoprefixer");
+attrsSorter = require("posthtml-attrs-sorter");
+del = require("del");
+reload = browserSync.reload;
 
-min = true
+min = true;
 
 bases = {
-  src: 'src',
-  dist: 'dist'
-}
+  src: "src",
+  dist: "dist",
+};
 
 path = {
   // proxy: 'local.dev/my/server/lol',
-  bootstrap_sass: 'assets/scss/vendor/bootstrap',
-  server: './',
-  img: 'assets/img',
-  scss: 'assets/scss',
-  js: 'assets/js',
-  css: 'assets/css',
-  fonts: 'assets/fonts',
+  bootstrap_sass: "assets/scss/vendor/bootstrap",
+  server: "./",
+  img: "assets/img",
+  scss: "assets/scss",
+  js: "assets/js",
+  css: "assets/css",
+  fonts: "assets/fonts",
   refresh: [
-    bases.src + '/' + 'assets/js' + '/**/*.js',
-    bases.src + '/' + '**/*.html',
-    bases.src + '/' + '**/*.php'
-  ]
-}
+    bases.src + "/" + "assets/js" + "/**/*.js",
+    bases.src + "/" + "**/*.html",
+    bases.src + "/" + "**/*.php",
+  ],
+};
 
 opts = {
   notify: false,
   open: true,
-  files: [bases.src + '/' + path.refresh]
-}
+  files: [bases.src + "/" + path.refresh],
+};
 
 posthtmlConfig = {
   plugins: [
     attrsSorter({
       order: [
-        'class',
-        'id',
-        'name',
-        'data',
-        'ng',
-        'src',
-        'for',
-        'type',
-        'href',
-        'values',
-        'title',
-        'alt',
-        'role',
-        'aria'
-      ]
-    })
+        "class",
+        "id",
+        "name",
+        "data",
+        "ng",
+        "src",
+        "for",
+        "type",
+        "href",
+        "values",
+        "title",
+        "alt",
+        "role",
+        "aria",
+      ],
+    }),
   ],
-  options: {}
-}
+  options: {},
+};
 
 // clean html
-gulp.task('html', function () {
+gulp.task("html", function () {
   return gulp
-    .src(bases.src + '/' + '**/*.html')
+    .src(bases.src + "/" + "**/*.html")
     .pipe($.plumber())
     .pipe($.posthtml(posthtmlConfig.plugins, posthtmlConfig.options))
-    .pipe(gulp.dest(bases.src))
-})
+    .pipe(gulp.dest(bases.src));
+});
 
 // make a bootstrap file
-gulp.task('bootstrap', function () {
+gulp.task("bootstrap", function () {
   return gulp
-    .src(bases.src + '/' + path.bootstrap_sass + '/**/*.scss')
+    .src(bases.src + "/" + path.bootstrap_sass + "/**/*.scss")
     .pipe($.plumber())
-    .pipe($.sass().on('error', $.sass.logError))
-    .pipe(gulp.dest(bases.src + '/' + path.css + '/vendor'))
-})
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(bases.src + "/" + path.css + "/vendor"));
+});
 
 // compile scss
-gulp.task('styles', function () {
+gulp.task("styles", function () {
   return gulp
-    .src(bases.src + '/' + path.scss + '/*.scss')
+    .src(bases.src + "/" + path.scss + "/*.scss")
     .pipe($.plumber())
-    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest(bases.src + '/' + path.css + '/'))
+    .pipe(gulp.dest(bases.src + "/" + path.css + "/"))
     .pipe(
       reload({
-        stream: true
+        stream: true,
       })
-    )
-})
+    );
+});
 
 // Minify css
-gulp.task('min-css', function () {
+gulp.task("min-css", function () {
   return gulp
     .src([
-      bases.src + '/' + path.css + '/vendor/bootstrap.css',
-      bases.src + '/' + path.css + '/main.css'
+      bases.src + "/" + path.css + "/vendor/bootstrap.css",
+      bases.src + "/" + path.css + "/main.css",
     ])
     .pipe($.plumber())
-    .pipe($.concat('main.css'))
-    .pipe(gulp.dest(bases.dist + '/' + path.css + '/'))
-    .pipe($.if(min, $.rename('all.min.css')))
+    .pipe($.concat("main.css"))
+    .pipe(gulp.dest(bases.dist + "/" + path.css + "/"))
+    .pipe($.if(min, $.rename("all.min.css")))
     .pipe(
       $.if(
         min,
         $.cssnano({
           discardComments: {
-            removeAll: true
-          }
+            removeAll: true,
+          },
         })
       )
     )
-    .pipe($.if(min, gulp.dest(bases.dist + '/' + path.css + '/')))
-})
+    .pipe($.if(min, gulp.dest(bases.dist + "/" + path.css + "/")));
+});
 
 // Minify js
-gulp.task('min-js', function () {
+gulp.task("min-js", function () {
   return gulp
     .src([
       // bases.src + '/' + path.js + '/lib/YOURJS.js',
-      bases.src + '/' + path.js + '/main.js'
+      bases.src + "/" + path.js + "/main.js",
     ])
     .pipe($.plumber())
-    .pipe($.concat('main.js'))
-    .pipe(gulp.dest(bases.dist + '/' + path.js + '/'))
-    .pipe($.if(min, $.rename('all.min.js')))
-    .pipe(
-      $.if(
-        min,
-        $.uglify()
-      )
-    )
-    .pipe($.if(min, gulp.dest(bases.dist + '/' + path.js + '/')))
-})
+    .pipe($.concat("main.js"))
+    .pipe(gulp.dest(bases.dist + "/" + path.js + "/"))
+    .pipe($.if(min, $.rename("all.min.js")))
+    .pipe($.if(min, $.uglify()))
+    .pipe($.if(min, gulp.dest(bases.dist + "/" + path.js + "/")));
+});
 
 // Clean folder dist
-gulp.task('clean', function () {
-  return del([bases.dist])
-})
+gulp.task("clean", function () {
+  return del([bases.dist]);
+});
 
 // Copy files into dist
-gulp.task('copy', function () {
+gulp.task("copy", function () {
   return gulp
     .src([
-      bases.src + '/**/*.html',
-      bases.src + '/favicon.ico',
-      bases.src + '/robots.txt',
-      bases.src + '/manifest.webmanifest'
+      bases.src + "/**/*.html",
+      bases.src + "/favicon.ico",
+      bases.src + "/robots.txt",
+      bases.src + "/manifest.webmanifest",
     ])
     .pipe($.plumber())
-    .pipe(gulp.dest(bases.dist))
-})
+    .pipe(gulp.dest(bases.dist));
+});
 
-gulp.task('watch', function () {
+gulp.task("watch", function () {
   if (path.proxy) {
-    opts.proxy = path.proxy
+    opts.proxy = path.proxy;
   } else {
     opts.server = {
-      baseDir: path.server + '/' + bases.src
-    }
+      baseDir: path.server + "/" + bases.src,
+    };
   }
-  browserSync(opts)
-
-
+  browserSync(opts);
 
   gulp.watch(
-    [bases.src + '/' + path.scss + '/**/*.scss'],
-    gulp.series('bootstrap', 'styles')
-  )
-  return $.watch(path.refresh, reload)
-})
+    [bases.src + "/" + path.scss + "/**/*.scss"],
+    gulp.series("styles")
+  );
+  return $.watch(path.refresh, reload);
+});
 
-gulp.task('default', gulp.series('html', 'watch', function () {}))
 gulp.task(
-  'prod',
+  "default",
+  gulp.series("bootstrap", "html", "watch", function () {})
+);
+gulp.task(
+  "prod",
   gulp.series(
-    'clean',
-    'bootstrap',
-    'styles',
-    'html',
-    'copy',
-    gulp.parallel('min-css', 'min-js'),
+    "clean",
+    "bootstrap",
+    "styles",
+    "html",
+    "copy",
+    gulp.parallel("min-css", "min-js")
   )
-)
+);
